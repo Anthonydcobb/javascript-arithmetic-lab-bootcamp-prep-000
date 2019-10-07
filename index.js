92abf0a6ca55f4778e252fa07158bbac7bccb5fe
Anthony David Cobb <anthonydcobb@gmail.com>
2019-10-07 00:37:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-var add(a, b) => {return a + b};
-var subtract(a, b) => {return a - b};
-var multiply(a, b) => {return a * b};
-var divide(a, b) => {return a / b};
+var add = (a, b) => {return a + b};
+var subtract = (a, b) => {return a - b};
+var multiply = (a, b) => {return a * b};
+var divide = (a, b) => {return a / b};
 
 var inc = (n) => {return n++};
 var dec = (n) => {return n--};
